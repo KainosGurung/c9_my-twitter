76254429469202bee1b29f5831e7734013b2a4a6
Kainos Gurung <kainosgurung@gmail.com>
2015-11-05 22:46:15 +0000

    Added tweets controller/model, Created new migration to add username to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151105204333) do
+ActiveRecord::Schema.define(version: 20151105224103) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.integer  "user_id",    limit: 4
+    t.string   "content",    limit: 255
+    t.integer  "likes",      limit: 4
+    t.integer  "dislikes",   limit: 4
+    t.integer  "retweets",   limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -26,6 +36,7 @@ ActiveRecord::Schema.define(version: 20151105204333) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "username",               limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
